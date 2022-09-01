0b34a1f8f45efb7f6d50ccaf87a4aff6102f1574
v.adeshkin <witPo8-myxduv-vajxew>
2022-09-01 20:00:07 +0300

    baseline

--- data/combine_data.py
+++ data/combine_data.py
@@ -2,60 +2,58 @@ import os
 import random
 
 
-def read_pairs(sample_dir, k_lang, split='train'):
-    k_filepath = f'{sample_dir}/{split}.{k_lang}'
-    with open(k_filepath, "r", encoding="utf-8") as f:
-        k_sents = f.readlines()
+def read_pairs(sample_dir, src_lang, tgt_lang, split):
+    src_filepath = f'{sample_dir}/{split}.{src_lang}'
+    with open(src_filepath, "r", encoding="utf-8") as f:
+        src_sents = f.readlines()
 
-    r_filepath = f'{sample_dir}/{split}.ru'
-    with open(r_filepath, "r", encoding="utf-8") as f:
-        r_sents = f.readlines()
+    tgt_filepath = f'{sample_dir}/{split}.{tgt_lang}'
+    with open(tgt_filepath, "r", encoding="utf-8") as f:
+        tgt_sents = f.readlines()
 
-    assert len(k_sents) == len(r_sents)
-    pairs = [(k_sent, r_sent) for k_sent, r_sent in zip(k_sents, r_sents)]
+    assert len(src_sents) == len(tgt_sents)
+    pairs = [(src_sent, tgt_sent) for src_sent, tgt_sent in zip(src_sents, tgt_sents)]
     return pairs
 
 
-def write_pairs(pairs, save_dir, k_lang, split='train'):
+def write_pairs(pairs, save_dir, src_lang, tgt_lang, split):
     os.makedirs(save_dir, exist_ok=True)
-    k_filepath = f'{save_dir}/{split}.{k_lang}'
-    r_filepath = f'{save_dir}/{split}.ru'
-    with open(k_filepath, "w", encoding="utf-8") as k_f, \
-            open(r_filepath, "w", encoding="utf-8") as r_f:
-        for k_sent, r_sent in pairs:
-            k_f.write(f"{k_sent.strip()}\n")
-            r_f.write(f"{r_sent.strip()}\n")
+    src_filepath = f'{save_dir}/{split}.{src_lang}'
+    tgt_filepath = f'{save_dir}/{split}.{tgt_lang}'
+    with open(src_filepath, "w", encoding="utf-8") as src_f, \
+            open(tgt_filepath, "w", encoding="utf-8") as tgt_f:
+        for src_sent, tgt_sent in pairs:
+            src_f.write(f"{src_sent.strip()}\n")
+            tgt_f.write(f"{tgt_sent.strip()}\n")
 
 
 def combine_data():
-    data_dir = 'tok_data'
+    random.seed(42)
+
+    sample_dir1 = 'tok_data/kjh_til_kk_ru'
+    src_lang1 = 'kjh_kk'
+    tgt_lang1 = 'ru'
+
+    sample_dir2 = 'tok_data/til_ky_ru'
+    src_lang2 = 'ky'
+    tgt_lang2 = 'ru'
+
+    save_dir = 'tok_data/kjh_til_kk_ky_ru'
+    src_lang = 'kjh_kk_ky'
+    tgt_lang = 'ru'
+
     for split in ['train', 'val', 'test']:
-        kjh_ru_pairs = read_pairs(sample_dir=f'{data_dir}/kjh_ru', k_lang='kjh', split=split)
-        kk_ru_pairs = read_pairs(sample_dir=f'{data_dir}/kk_ru', k_lang='kk', split=split)
-        ky_ru_pairs = read_pairs(sample_dir=f'{data_dir}/ky_ru', k_lang='ky', split=split)
-        source_kk_ru_pairs = read_pairs(sample_dir=f'{data_dir}/source_kk_ru', k_lang='kk', split=split)
+        pairs1 = read_pairs(sample_dir=sample_dir1, src_lang=src_lang1, tgt_lang=tgt_lang1, split=split)
+        pairs2 = read_pairs(sample_dir=sample_dir2, src_lang=src_lang2, tgt_lang=tgt_lang2, split=split)
 
-        kk_kjh_pairs = kjh_ru_pairs + kk_ru_pairs
-        ky_kjh_pairs = kjh_ru_pairs + ky_ru_pairs
-        source_kk_kjh_pairs = kjh_ru_pairs + source_kk_ru_pairs
+        pairs = pairs1 + pairs2
 
-        print(len(kjh_ru_pairs), len(kk_ru_pairs), len(ky_ru_pairs), len(source_kk_ru_pairs))
-        print(len(kk_kjh_pairs), len(ky_kjh_pairs), len(source_kk_kjh_pairs))
+        random.shuffle(pairs)
+        
+        print(len(pairs))
+        print(pairs[:2])
 
-        random.seed(42)
-        random.shuffle(kk_kjh_pairs)
-        random.seed(42)
-        random.shuffle(ky_kjh_pairs)
-        random.seed(42)
-        random.shuffle(source_kk_kjh_pairs)
-
-        print(kk_kjh_pairs[:2])
-        print(ky_kjh_pairs[:2])
-        print(source_kk_kjh_pairs[:2])
-
-        write_pairs(kk_kjh_pairs, save_dir=f'{data_dir}/kjh_kk_ru', k_lang='kjh_kk', split=split)
-        write_pairs(ky_kjh_pairs, save_dir=f'{data_dir}/kjh_ky_ru', k_lang='kjh_ky', split=split)
-        write_pairs(source_kk_kjh_pairs, save_dir=f'{data_dir}/kjh_source_kk_ru', k_lang='kjh_kk', split=split)
+        write_pairs(pairs, save_dir=save_dir, src_lang=src_lang, tgt_lang=tgt_lang, split=split)
 
 
 if __name__ == '__main__':
