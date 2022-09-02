86151f143a4a3e77f2b6c2090b628aa332924242
v.adeshkin <witPo8-myxduv-vajxew>
2022-09-03 00:10:08 +0300

    baseline

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 
 
-
-for config in config/kjh_ky_ru/ru_ky*
+for config in config/kjh_wmt19_kk_til_ky_ru/??/ru_??.*
 do
   echo $config
   python main.py $config
 done
 
-for config in config/kjh_ky_ru/ru_kjh*
+for config in config/kjh_wmt19_kk_til_ky_ru/??/ru_???.*
 do
   echo $config
   python main.py $config
