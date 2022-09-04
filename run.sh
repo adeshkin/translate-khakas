3b94b27e69b1f8e669f73db589a3dff6414c0ce0
v.adeshkin <witPo8-myxduv-vajxew>
2022-09-04 07:36:07 +0300

    baseline

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 
-# train
-for config in config/kjh_til_kk_ru/??/ru_??.*
-do
-  python main.py $config
-done
-
-# finetune
-for config in config/kjh_til_kk_ru/??/ru_???.*
-do
-  python main.py $config
-done
+## train
+#for config in config/kjh_til_kk_ru/??/ru_??.*
+#do
+#  python main.py $config
+#done
+#
+## finetune
+#for config in config/kjh_til_kk_ru/??/ru_???.*
+#do
+#  python main.py $config
+#done
 
 # train scratch kjh
 for config in config/kjh_til_kk_ru/???/*
