7aba47fa39f7bd8fee696c09b77d57596b8e993b
v.adeshkin <witPo8-myxduv-vajxew>
2022-09-06 11:55:52 +0300

    baseline

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 
 # train
-for config in config/dict_kjh_wmt19_thr_2_kk_ru/??/ru_??.*
-do
-  python main.py $config
-done
-
-# finetune
-for config in config/dict_kjh_wmt19_thr_2_kk_ru/??/ru_???.*
-do
-  python main.py $config
-done
+#for config in config/dict_kjh_wmt19_thr_2_kk_ru/??/ru_??.*
+#do
+#  python main.py $config
+#done
+#
+## finetune
+#for config in config/dict_kjh_wmt19_thr_2_kk_ru/??/ru_???.*
+#do
+#  python main.py $config
+#done
 
 # train scratch kjh
 for config in config/dict_kjh_wmt19_thr_2_kk_ru/???/*
