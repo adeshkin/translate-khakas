611b8ddc963d729ad0de81a516ef9d2227024b06
v.adeshkin <witPo8-myxduv-vajxew>
2022-09-09 23:03:11 +0300

    baseline

--- inference/onnx_inference.py
+++ inference/onnx_inference.py
@@ -1,6 +1,18 @@
 import onnxruntime
 
 
+def custom_greedy_decode(src_sentence, device):
+    src = text_transform[SRC_LANGUAGE](src_sentence).view(-1, 1)
+    num_tokens = src.shape[0]
+    src_mask = (torch.zeros(num_tokens, num_tokens)).type(torch.bool)
+
+    src = src.to(device)
+    src_mask = src_mask.to(device)
+
+    max_len = num_tokens + 5
+    start_symbol = BOS_IDX
+
+
 def main():
     onnx_dir = ''
     encoder_sess = onnxruntime.InferenceSession(f'{onnx_dir}/model_encoder.onnx')
