3b237338d5378bead9438ddf376ddb47210d2da1
v.adeshkin <witPo8-myxduv-vajxew>
2022-09-10 15:35:39 +0300

    baseline

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 
-# train
-for config in config/kjh_wmt19_thr_2_kk_ru/??/ru_??.*
-do
-  python main.py $config
-done
+## train
+#for config in config/kjh_wmt19_thr_2_kk_ru/??/ru_??.*
+#do
+#  python main.py $config
+#done
 
 # finetune
 for config in config/kjh_wmt19_thr_2_kk_ru/??/ru_???.*
@@ -11,8 +11,8 @@ do
   python main.py $config
 done
 
-# train scratch kjh
-for config in config/kjh_wmt19_thr_2_kk_ru/???/*
-do
-  python main.py $config
-done
+## train scratch kjh
+#for config in config/kjh_wmt19_thr_2_kk_ru/???/*
+#do
+#  python main.py $config
+#done
